49056ad868995bfba8e41deb2931c69e2859e1fa
Skye Yeh <presto408@gmail.com>
2016-05-09 14:40:53 +0800

    Add comment.

--- app/src/main/java/com/skyeyeh/androidexample/MainActivity.java
+++ app/src/main/java/com/skyeyeh/androidexample/MainActivity.java
@@ -1,13 +1,23 @@
 package com.skyeyeh.androidexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
+/**
+ * 繼承自 Activity 類別。
+ */
 public class MainActivity extends AppCompatActivity {
-
+    /**
+     * 覆寫 Activity 類別的 onCreate 方法。
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // 一定要加入呼叫父類別 onCreate 方法的敘述
         super.onCreate(savedInstanceState);
+
+        // 指定這個元件使用的畫面配置資源
         setContentView(R.layout.activity_main);
     }
 }
