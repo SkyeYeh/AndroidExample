5f63f1dd6d83c652647f7bca5d49a5c918c97973
SkyeYeh <presto408@gmail.com>
2016-05-22 16:19:08 +0800

    Add main_menu.

--- app/src/main/java/com/skyeyeh/androidexample/MainActivity.java
+++ app/src/main/java/com/skyeyeh/androidexample/MainActivity.java
@@ -2,6 +2,7 @@ package com.skyeyeh.androidexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 /**
  * 繼承自 Activity 類別。
@@ -19,5 +20,18 @@ public class MainActivity extends AppCompatActivity {
 
         // 指定這個元件使用的畫面配置資源
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * 加入載入選單資源的方法。
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+//        return super.onCreateOptionsMenu(menu);
     }
 }
